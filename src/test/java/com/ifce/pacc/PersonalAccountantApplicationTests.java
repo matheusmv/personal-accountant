e9e812f1a8413d5c02d41a459a54eb70725e1613
Matheus Matias <matheus.matias.viana@hotmail.com>
2021-02-09 17:45:59 -0300

    project started

---
+++ src/test/java/com/ifce/pacc/PersonalAccountantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifce.pacc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalAccountantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
