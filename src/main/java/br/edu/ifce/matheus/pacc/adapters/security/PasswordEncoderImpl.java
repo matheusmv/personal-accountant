7c9ffb9571814d0f1ae75c0c9e0a59c3e00f8bf0
Matheus Matias <matheus.matias.viana@hotmail.com>
2021-05-11 13:28:38 -0300

    adding password encode

---
+++ src/main/java/br/edu/ifce/matheus/pacc/adapters/security/PasswordEncoderImpl.java
@@ -0,0 +1,18 @@
+package br.edu.ifce.matheus.pacc.adapters.security;
+
+import br.edu.ifce.matheus.pacc.domain.ports.PasswordEncoder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PasswordEncoderImpl implements PasswordEncoder {
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Override
+    public String encode(String password) {
+        return bCryptPasswordEncoder.encode(password);
+    }
+}
