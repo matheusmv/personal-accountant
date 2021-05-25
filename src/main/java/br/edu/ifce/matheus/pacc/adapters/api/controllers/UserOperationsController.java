11817b2fd94ebb248b259375ccdb70da80a98e9f
Matheus Matias <matheus.matias.viana@hotmail.com>
2021-05-25 10:46:15 -0300

    get all users - implementing basic pagination

--- src/main/java/br/edu/ifce/matheus/pacc/adapters/api/controllers/UserOperationsController.java
+++ src/main/java/br/edu/ifce/matheus/pacc/adapters/api/controllers/UserOperationsController.java
@@ -1,18 +1,17 @@
 package br.edu.ifce.matheus.pacc.adapters.api.controllers;
 
+import br.edu.ifce.matheus.pacc.adapters.api.controllers.responses.UserPageableResponse;
 import br.edu.ifce.matheus.pacc.adapters.api.controllers.responses.UserProfileResponse;
 import br.edu.ifce.matheus.pacc.domain.ports.driver.GetAUser;
 import br.edu.ifce.matheus.pacc.domain.ports.driver.GetAllUsers;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @RestController
 @RequestMapping("/api/v1/users")
@@ -23,11 +22,11 @@ public class UserOperationsController {
     private final GetAUser getAUser;
 
     @GetMapping
-    public ResponseEntity<List<UserProfileResponse>> getAllUsers() {
-        // TODO: implement pagination and ordering
-        var listOfUsers = getAllUsers.execute()
-                .stream()
-                .map(UserProfileResponse::new)
+    public ResponseEntity<List<UserPageableResponse>> getAllUsers(@RequestParam(defaultValue = "0") int page,
+                                                                  @RequestParam(defaultValue = "3") int size) {
+        var pageOfUsers = getAllUsers.execute(page, size);
+        var listOfUsers = Stream.ofNullable(pageOfUsers)
+                .map(UserPageableResponse::new)
                 .collect(Collectors.toList());
         return new ResponseEntity<>(listOfUsers, HttpStatus.OK);
     }
--- src/main/java/br/edu/ifce/matheus/pacc/domain/services/GetAllUsersService.java
+++ src/main/java/br/edu/ifce/matheus/pacc/domain/services/GetAllUsersService.java
@@ -4,9 +4,10 @@ import br.edu.ifce.matheus.pacc.domain.entities.User;
 import br.edu.ifce.matheus.pacc.domain.ports.driven.UserRepository;
 import br.edu.ifce.matheus.pacc.domain.ports.driver.GetAllUsers;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 @AllArgsConstructor
@@ -15,7 +16,8 @@ public class GetAllUsersService implements GetAllUsers {
     private final UserRepository userRepository;
 
     @Override
-    public List<User> execute() {
-        return userRepository.findAll();
+    public Page<User> execute(int page, int size) {
+        Pageable paging = PageRequest.of(page, size);
+        return userRepository.findAllUsers(paging);
     }
 }
